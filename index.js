650c86801962c00739c6da5be84ed8033f36117d
alvarosiac <137443745+alvarosiac@users.noreply.github.com>
2024-01-07 15:58:38 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const aedes = require("aedes")();
-const httpServer = require("http").createServer((request, response) => {
-  response.end("hello");
-});
+const httpServer = require("http").createServer();
 const ws = require("websocket-stream");
 const port = process.env.PORT || 80;
 
 ws.createServer({ server: httpServer }, aedes.handle);
 
-httpServer.listen(port, () => {
-  console.log("websocket ", port);
+httpServer.listen(port, (request, response) => {
+  response.end(`Puerto ${port}`);
 });
